51fef6bb6f6f96d7c275335bc005f618bd9c29d9
Cheshire Cat <thacheshirecat@gmail.com>
2018-05-30 12:00:22 -0700

    Add buttons to change the background and text color.

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,21 @@ $("#no-more-zoot").click(function(){
   $("#extra-zoot").toggle();
 })
 
+$("button#display1").click(function(){
+  $("body").removeClass();
+  $("body").addClass("style1")
+})
+
+$("button#display2").click(function(){
+  $("body").removeClass();
+  $("body").addClass("style2")
+})
+
+$("button#displayC").click(function(){
+  $("body").removeClass();
+  $("body").addClass("styleC")
+})
+
 
 
 
